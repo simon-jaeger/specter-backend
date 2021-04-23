67b90ebebe1f5066aadec63eb3bc87f6066ffedf
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-04-23 11:32:58 +0200

    skip csrf check in local environment

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Middleware;
 
+use App;
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
-class VerifyCsrfToken extends Middleware
-{
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        //
-    ];
+class VerifyCsrfToken extends Middleware {
+  /**
+   * The URIs that should be excluded from CSRF verification.
+   *
+   * @var array
+   */
+  protected $except = [
+    //
+  ];
+
+  // skip csrf check in dev environment
+  public function handle($request, Closure $next) {
+    if (App::environment(['local', 'dev'])) {
+      return $next($request);
+    }
+
+    return parent::handle($request, $next);
+  }
 }
