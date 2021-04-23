fba88305fcfba58924ef196cfdb64f7e6a7db3c5
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-04-23 11:59:39 +0200

    flatten validation error response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,39 +3,46 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
-class Handler extends ExceptionHandler
-{
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+class Handler extends ExceptionHandler {
+  /**
+   * A list of the exception types that are not reported.
+   *
+   * @var array
+   */
+  protected $dontReport = [
+    //
+  ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * A list of the inputs that are never flashed for validation exceptions.
+   *
+   * @var array
+   */
+  protected $dontFlash = [
+    'current_password',
+    'password',
+    'password_confirmation',
+  ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+  /**
+   * Register the exception handling callbacks for the application.
+   *
+   * @return void
+   */
+  public function register() {
+    $this->reportable(function (Throwable $e) {
+      //
+    });
+  }
+
+  // flatten validation errors, only return first for each field
+  protected function invalidJson($request, ValidationException $exception) {
+    return response()->json([
+      'message' => $exception->getMessage(),
+      'errors' => collect($exception->errors())->map(fn($x) => $x[0])
+    ], $exception->status);
+  }
 }
