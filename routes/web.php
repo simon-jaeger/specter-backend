438800699b804ad1dafb883512257db99d20026a
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-04-26 11:23:04 +0200

    users: avatar url in api response

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::prefix('api')->middleware('throttle:90,1')->group(function () {
   Route::patch('/user', [UserController::class, 'update'])->middleware('auth');
   Route::get('/users', [UserController::class, 'index']);
   Route::get('/users/{user}', [UserController::class, 'show']);
-  Route::get('/users/{user}/avatar', [UserController::class, 'avatar']);
+  Route::get('/users/{user}/avatar', [UserController::class, 'avatar'])->name('users.avatar');
 });
