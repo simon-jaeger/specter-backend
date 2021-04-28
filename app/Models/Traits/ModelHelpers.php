cc801484af73310cc90a070c47d55d4d86b7478a
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-04-28 15:09:47 +0200

    new model helpers

--- app/Models/Traits/ModelHelpers.php
+++ app/Models/Traits/ModelHelpers.php
@@ -2,8 +2,22 @@
 
 namespace App\Models\Traits;
 
+use Illuminate\Support\Str;
+
 trait ModelHelpers {
   public static function table() {
     return self::make()->getTable();
+  }
+
+  public static function foreignKey() {
+    return self::make()->getForeignKey();
+  }
+
+  public static function singular() {
+    return Str::snake(class_basename(self::make()));
+  }
+
+  public static function plural() {
+    return Str::plural(self::singular());
   }
 }
