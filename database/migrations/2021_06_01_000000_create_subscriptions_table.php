7e17407afa0adba8428887840ab69aaba715b41c
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-06-10 14:25:10 +0200

    fix: get subscriberKey from Subscription model

--- database/migrations/2021_06_01_000000_create_subscriptions_table.php
+++ database/migrations/2021_06_01_000000_create_subscriptions_table.php
@@ -10,7 +10,7 @@ class CreateSubscriptionsTable extends Migration {
   public function up() {
     Schema::create(Subscription::table(), function (Blueprint $table) {
       $table->id();
-      $table->foreignId(User::subscriberKey())->constrained(User::table());
+      $table->foreignId(Subscription::subscriberKey)->constrained(User::table());
       $table->foreignId(User::foreignKey())->constrained();
       $table->timestamps();
     });
